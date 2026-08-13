aed0fbd6e0f05b41579a9183c1152799a200c1b4
Sean Sovine <sean.r.sovine@gmail.com>
2026-08-13 08:34:39 -0400

    Organize code some and add logging

--- accel_client/src/main.rs
+++ accel_client/src/main.rs
@@ -1,22 +1,61 @@
-use std::{env, time::Duration};
+use std::{env, error::Error, fmt::Write, thread, time::Duration};
+
+use serialport::SerialPort;
+use time::macros::format_description;
+use tracing::{debug, error, info, warn};
+use tracing_subscriber::{EnvFilter, fmt::time::UtcTime};
 
 fn main() {
-    let device = env::var("ACCEL_DEVICE")
-        .expect("Environment variable ACCEL_DEVICE must be set to a valid ttyACM device.");
+    // Setup logging.
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")),
+        )
+        .with_timer(UtcTime::new(format_description!(
+            "[hour]:[minute]:[second].[subsecond digits:6]"
+        )))
+        .init();
 
-    let baud_rate = 1_000_000;
-    let mut port = match serialport::new(&device, baud_rate)
+    let Ok(device) = env::var("ACCEL_DEVICE") else {
+        error!("Environment variable ACCEL_DEVICE must be set to a valid ttyACM device.");
+        std::process::exit(1);
+    };
+    let baud_rate = 1_000_000_u32;
+
+    loop {
+        let mut port = match try_connect(&device, baud_rate) {
+            Ok(p) => p,
+            Err(e) => {
+                error!("Failed to open device {}: {}", device, e);
+                thread::sleep(Duration::from_secs(2));
+                continue;
+            }
+        };
+
+        info!("Reading from device {}.", device);
+        receive(&mut *port);
+    }
+}
+
+fn try_connect(device_name: &str, baud_rate: u32) -> Result<Box<dyn SerialPort>, Box<dyn Error>> {
+    match serialport::new(device_name, baud_rate)
         .timeout(Duration::from_millis(1000))
         .open()
     {
-        Ok(p) => p,
-        Err(e) => {
-            eprintln!("Failed to open device {}: {}", device, e);
-            std::process::exit(1);
-        }
-    };
-    println!("Reading from device {}.", device);
+        Ok(p) => Ok(p),
+        Err(e) => Err(Box::from(e)),
+    }
+}
 
+fn byte_string(bytes: &[u8]) -> String {
+    let mut s = String::with_capacity(bytes.len() * 5);
+    for byte in bytes {
+        write!(&mut s, "{:#04X} ", byte).unwrap();
+    }
+    s
+}
+
+fn receive(port: &mut dyn SerialPort) {
     let mut serial_buf: Vec<u8> = vec![0; 1024];
     let mut read_end = 0;
 
@@ -32,26 +71,20 @@ fn main() {
         match port.read(&mut serial_buf) {
             Ok(t) => {
                 read_end += t;
-                print!("Current buffer: ");
-                for byte in &serial_buf[..t] {
-                    print!("{:#04X} ", byte);
-                }
-                println!();
+                let s = byte_string(&serial_buf[..t]);
+                debug!("Current buffer: {s}");
             }
             Err(e) => {
-                eprintln!("Error reading data: {}", e);
+                error!("Error reading data: {}", e);
                 break;
             }
         }
 
-        /*
-         * Simple state machine to handle reading our data packet format.
-         * See comments in the board C code for details.
-         *
-         * Not really necessayr for usb serial, except for the case that we
-         * start receiving in the middle of a message or several messages are
-         * currently buffered by the OS.
-         */
+        // Simple state machine to read a stream of our data packets from
+        // a serial port. See comments in the board C code for details.
+        //
+        // Currently used for USB ACM comm port, but can be used for more
+        // general serial connections.
 
         let mut escaped = false;
         for &byte in &serial_buf[0..read_end] {
@@ -69,7 +102,7 @@ fn main() {
                     }
                     if escaped {
                         if byte != 0x00 && byte != 0xFF {
-                            println!("Invalid escape sequence.");
+                            warn!("Invalid escape sequence. Dropping current packet.");
                             reader_state = ReadState::NotStarted;
                         } else {
                             current_valid.push(byte);
@@ -85,14 +118,18 @@ fn main() {
                 }
                 ReadState::FirstStopSeen => {
                     if byte == 0x00 {
-                        print!("Received packet: ");
-                        for byte in &current_valid {
-                            print!("{:#04X} ", byte);
+                        let s = byte_string(&current_valid);
+                        debug!("Received packet: {s}");
+                        if let Some(reading) = process_packet(&current_valid) {
+                            reading.log_to_info();
+                        } else {
+                            warn!(
+                                "Invalid packet length: {}. Packet was dropped.",
+                                current_valid.len()
+                            );
                         }
-                        println!();
-                        process_packet(&current_valid);
                     } else {
-                        println!("Invalid stop character received.");
+                        warn!("Invalid stop character received. Dropping current packet.");
                     }
                     reader_state = ReadState::NotStarted;
                 }
@@ -102,32 +139,46 @@ fn main() {
     }
 }
 
-fn process_packet(data: &[u8]) {
+struct Reading {
+    pub a_x_g: f32,
+    pub a_y_g: f32,
+    pub a_z_g: f32,
+}
+
+impl Reading {
+    fn log_to_info(&self) {
+        info!("Current reading:");
+        info!("Scaled A_x = {:+1.5}g ", self.a_x_g,);
+        info!("Scaled A_y = {:+1.5}g", self.a_y_g);
+        info!("Scaled A_Z = {:+1.5}g", self.a_z_g);
+    }
+}
+
+fn process_packet(data: &[u8]) -> Option<Reading> {
     if data.len() != 6 {
-        println!("Invalid packet length.");
-        return;
+        return None;
     }
 
-    /*
-     * Calibration constants, determined by experiment.
-     * TODO: Add a calibration mode that writes a file.
-     */
+    // Calibration constants, determined by experiment.
+    //
+    // TODO: Add a calibration mode that writes a file.
 
     const G_SCALE_X: f32 = 17_700.0;
     const G_SCALE_Y: f32 = 16_500.0;
     const G_SCALE_Z: f32 = 17_300.0;
 
     let a_x = (((data[0] as u16) << 8) | (data[1] as u16)) as i16;
-    let a_x_f = (a_x as f32) / G_SCALE_X;
+    let a_x_g = (a_x as f32) / G_SCALE_X;
 
     let a_y = (((data[2] as u16) << 8) | (data[3] as u16)) as i16;
-    let a_y_f = (a_y as f32) / G_SCALE_Y;
+    let a_y_g = (a_y as f32) / G_SCALE_Y;
 
     let a_z = (((data[4] as u16) << 8) | (data[5] as u16)) as i16;
-    let a_z_f = (a_z as f32) / G_SCALE_Z;
+    let a_z_g = (a_z as f32) / G_SCALE_Z;
 
-    println!("Current reading:");
-    println!("Scaled A_x = {:+1.5}g ", a_x_f,);
-    println!("Scaled A_y = {:+1.5}g", a_y_f);
-    println!("Scaled A_Z = {:+1.5}g", a_z_f);
+    Some(Reading {
+        a_x_g,
+        a_y_g,
+        a_z_g,
+    })
 }
