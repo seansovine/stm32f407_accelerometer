32e24d9e074b063f3233e046de06012d5be4d9fe
Sean Sovine <sean.r.sovine@gmail.com>
2026-08-13 20:13:15 -0400

    Run serial data reader in separate thread

---
+++ accel_client/src/lib.rs
@@ -0,0 +1,205 @@
+use std::{
+    error::Error,
+    fmt::Write,
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    },
+    thread::{self, JoinHandle},
+    time::Duration,
+};
+
+use serialport::SerialPort;
+use tracing::{error, error_span, info, info_span, trace, trace_span, warn, warn_span};
+use triple_buffer::{Input, Output, triple_buffer};
+
+#[derive(Clone, Copy, Default)]
+pub struct Reading {
+    pub a_x_g: f32,
+    pub a_y_g: f32,
+    pub a_z_g: f32,
+    pub valid: bool,
+}
+
+impl Reading {
+    pub fn debug_format(&self) -> String {
+        let mut s = String::with_capacity(21 * 3 + 9);
+        writeln!(&mut s, "Scaled A_x = {:+1.5}g", self.a_x_g).unwrap();
+        writeln!(&mut s, "Scaled A_y = {:+1.5}g", self.a_y_g).unwrap();
+        writeln!(&mut s, "Scaled A_Z = {:+1.5}g", self.a_z_g).unwrap();
+
+        let valid = if self.valid { "[valid]" } else { "[invalid]" };
+        write!(&mut s, "{valid}").unwrap();
+
+        s
+    }
+}
+
+pub fn run(device_name: String, stop: Arc<AtomicBool>) -> (Output<Reading>, JoinHandle<()>) {
+    let (mut buf_input, buf_output) = triple_buffer::<Reading>(&Default::default());
+
+    let handle = thread::Builder::new()
+        .spawn(move || {
+            let trace_span = trace_span!("reader_thread");
+            let _ = trace_span.enter();
+            let info_span = info_span!("reader_thread");
+            let _ = info_span.enter();
+            let error_span = error_span!("reader_thread");
+            let _ = error_span.enter();
+            let warn_span = warn_span!("reader_thread");
+            let _ = warn_span.enter();
+
+            let baud_rate = 1_000_000_u32;
+            while !stop.load(Ordering::Relaxed) {
+                let mut port = match try_connect(&device_name, baud_rate) {
+                    Ok(p) => p,
+                    Err(e) => {
+                        error!("Failed to open device {}: {}", device_name, e);
+                        error!("Retrying in 2 seconds...");
+                        thread::sleep(Duration::from_secs(2));
+                        continue;
+                    }
+                };
+
+                info!("Reading from device {}.", device_name);
+                receive(&mut *port, &mut buf_input, &stop);
+            }
+        })
+        .unwrap();
+
+    (buf_output, handle)
+}
+
+fn try_connect(device_name: &str, baud_rate: u32) -> Result<Box<dyn SerialPort>, Box<dyn Error>> {
+    match serialport::new(device_name, baud_rate)
+        .timeout(Duration::from_millis(1000))
+        .open()
+    {
+        Ok(p) => Ok(p),
+        Err(e) => Err(Box::from(e)),
+    }
+}
+
+fn byte_string(bytes: &[u8]) -> String {
+    let mut s = String::with_capacity(bytes.len() * 5);
+    for byte in bytes {
+        write!(&mut s, "{:#04X} ", byte).unwrap();
+    }
+    s
+}
+
+fn receive(port: &mut dyn SerialPort, buf_input: &mut Input<Reading>, stop: &AtomicBool) {
+    let mut serial_buf: Vec<u8> = vec![0; 1024];
+    let mut read_end = 0;
+
+    enum ReadState {
+        Started,
+        NotStarted,
+        FirstStopSeen,
+    }
+    let mut reader_state = ReadState::NotStarted;
+    let mut current_valid: Vec<u8> = Vec::with_capacity(12);
+
+    while !stop.load(Ordering::Relaxed) {
+        match port.read(&mut serial_buf) {
+            Ok(t) => {
+                read_end += t;
+                let s = byte_string(&serial_buf[..t]);
+                trace!("Current buffer: {s}");
+            }
+            Err(e) => {
+                error!("Error reading data: {}", e);
+                break;
+            }
+        }
+
+        // Simple state machine to read a stream of our data packets from
+        // a serial port. See comments in the board C code for details.
+        //
+        // Currently used for USB ACM comm port, but can be used for more
+        // general serial connections.
+
+        let mut escaped = false;
+        for &byte in &serial_buf[0..read_end] {
+            match reader_state {
+                ReadState::NotStarted => {
+                    if byte == 0x00 {
+                        reader_state = ReadState::Started;
+                        current_valid.clear();
+                    }
+                }
+                ReadState::Started => {
+                    if !escaped && byte == 0xFF {
+                        escaped = true;
+                        continue;
+                    }
+                    if escaped {
+                        if byte != 0x00 && byte != 0xFF {
+                            warn!("Invalid escape sequence. Dropping current packet.");
+                            reader_state = ReadState::NotStarted;
+                        } else {
+                            current_valid.push(byte);
+                        }
+                        escaped = false;
+                    } else {
+                        if byte == 0x00 {
+                            reader_state = ReadState::FirstStopSeen;
+                        } else {
+                            current_valid.push(byte);
+                        }
+                    }
+                }
+                ReadState::FirstStopSeen => {
+                    if byte == 0x00 {
+                        let s = byte_string(&current_valid);
+                        trace!("Received packet: {s}");
+                        if let Some(reading) = process_packet(&current_valid) {
+                            let s = reading.debug_format();
+                            trace!("Processed packet:\n{s}");
+                            buf_input.write(reading);
+                        } else {
+                            warn!(
+                                "Invalid packet length: {}. Packet was dropped.",
+                                current_valid.len()
+                            );
+                        }
+                    } else {
+                        warn!("Invalid stop character received. Dropping current packet.");
+                    }
+                    reader_state = ReadState::NotStarted;
+                }
+            }
+        }
+        read_end = 0;
+    }
+}
+
+fn process_packet(data: &[u8]) -> Option<Reading> {
+    if data.len() != 6 {
+        return None;
+    }
+
+    // Calibration constants, determined by experiment.
+    //
+    // TODO: Add a calibration mode that writes a file.
+
+    const G_SCALE_X: f32 = 17_700.0;
+    const G_SCALE_Y: f32 = 16_500.0;
+    const G_SCALE_Z: f32 = 17_300.0;
+
+    let a_x = (((data[0] as u16) << 8) | (data[1] as u16)) as i16;
+    let a_x_g = (a_x as f32) / G_SCALE_X;
+
+    let a_y = (((data[2] as u16) << 8) | (data[3] as u16)) as i16;
+    let a_y_g = (a_y as f32) / G_SCALE_Y;
+
+    let a_z = (((data[4] as u16) << 8) | (data[5] as u16)) as i16;
+    let a_z_g = (a_z as f32) / G_SCALE_Z;
+
+    Some(Reading {
+        a_x_g,
+        a_y_g,
+        a_z_g,
+        valid: true,
+    })
+}
--- accel_client/src/main.rs
+++ accel_client/src/main.rs
@@ -1,9 +1,17 @@
-use std::{env, error::Error, fmt::Write, thread, time::Duration};
+use std::{
+    env,
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    },
+    time::Duration,
+};
 
-use serialport::SerialPort;
 use time::macros::format_description;
-use tracing::{debug, error, info, warn};
+use tracing::{error, info};
 use tracing_subscriber::{EnvFilter, fmt::time::UtcTime};
+
+use accel_client::*;
 
 fn main() {
     // Setup logging.
@@ -14,171 +22,26 @@ fn main() {
         .with_timer(UtcTime::new(format_description!(
             "[hour]:[minute]:[second].[subsecond digits:6]"
         )))
+        .with_thread_ids(true)
         .init();
 
     let Ok(device) = env::var("ACCEL_DEVICE") else {
         error!("Environment variable ACCEL_DEVICE must be set to a valid ttyACM device.");
         std::process::exit(1);
     };
-    let baud_rate = 1_000_000_u32;
+    info!("Got device from environment: {device}");
 
-    loop {
-        let mut port = match try_connect(&device, baud_rate) {
-            Ok(p) => p,
-            Err(e) => {
-                error!("Failed to open device {}: {}", device, e);
-                thread::sleep(Duration::from_secs(2));
-                continue;
-            }
-        };
+    let stop = Arc::new(AtomicBool::new(false));
+    let (mut output_buf, handle) = run(device, stop.clone());
 
-        info!("Reading from device {}.", device);
-        receive(&mut *port);
-    }
-}
-
-fn try_connect(device_name: &str, baud_rate: u32) -> Result<Box<dyn SerialPort>, Box<dyn Error>> {
-    match serialport::new(device_name, baud_rate)
-        .timeout(Duration::from_millis(1000))
-        .open()
-    {
-        Ok(p) => Ok(p),
-        Err(e) => Err(Box::from(e)),
-    }
-}
-
-fn byte_string(bytes: &[u8]) -> String {
-    let mut s = String::with_capacity(bytes.len() * 5);
-    for byte in bytes {
-        write!(&mut s, "{:#04X} ", byte).unwrap();
-    }
-    s
-}
-
-fn receive(port: &mut dyn SerialPort) {
-    let mut serial_buf: Vec<u8> = vec![0; 1024];
-    let mut read_end = 0;
-
-    enum ReadState {
-        Started,
-        NotStarted,
-        FirstStopSeen,
-    }
-    let mut reader_state = ReadState::NotStarted;
-    let mut current_valid: Vec<u8> = Vec::with_capacity(12);
-
-    loop {
-        match port.read(&mut serial_buf) {
-            Ok(t) => {
-                read_end += t;
-                let s = byte_string(&serial_buf[..t]);
-                debug!("Current buffer: {s}");
-            }
-            Err(e) => {
-                error!("Error reading data: {}", e);
-                break;
-            }
+    for _ in 0..200 {
+        if handle.is_finished() {
+            break;
         }
-
-        // Simple state machine to read a stream of our data packets from
-        // a serial port. See comments in the board C code for details.
-        //
-        // Currently used for USB ACM comm port, but can be used for more
-        // general serial connections.
-
-        let mut escaped = false;
-        for &byte in &serial_buf[0..read_end] {
-            match reader_state {
-                ReadState::NotStarted => {
-                    if byte == 0x00 {
-                        reader_state = ReadState::Started;
-                        current_valid.clear();
-                    }
-                }
-                ReadState::Started => {
-                    if !escaped && byte == 0xFF {
-                        escaped = true;
-                        continue;
-                    }
-                    if escaped {
-                        if byte != 0x00 && byte != 0xFF {
-                            warn!("Invalid escape sequence. Dropping current packet.");
-                            reader_state = ReadState::NotStarted;
-                        } else {
-                            current_valid.push(byte);
-                        }
-                        escaped = false;
-                    } else {
-                        if byte == 0x00 {
-                            reader_state = ReadState::FirstStopSeen;
-                        } else {
-                            current_valid.push(byte);
-                        }
-                    }
-                }
-                ReadState::FirstStopSeen => {
-                    if byte == 0x00 {
-                        let s = byte_string(&current_valid);
-                        debug!("Received packet: {s}");
-                        if let Some(reading) = process_packet(&current_valid) {
-                            reading.log_to_info();
-                        } else {
-                            warn!(
-                                "Invalid packet length: {}. Packet was dropped.",
-                                current_valid.len()
-                            );
-                        }
-                    } else {
-                        warn!("Invalid stop character received. Dropping current packet.");
-                    }
-                    reader_state = ReadState::NotStarted;
-                }
-            }
-        }
-        read_end = 0;
+        std::thread::sleep(Duration::from_millis(1000 / 60));
+        let s = output_buf.read().debug_format();
+        info!("Packet in buffer: \n{s}");
     }
-}
-
-struct Reading {
-    pub a_x_g: f32,
-    pub a_y_g: f32,
-    pub a_z_g: f32,
-}
-
-impl Reading {
-    fn log_to_info(&self) {
-        info!("Current reading:");
-        info!("Scaled A_x = {:+1.5}g ", self.a_x_g,);
-        info!("Scaled A_y = {:+1.5}g", self.a_y_g);
-        info!("Scaled A_Z = {:+1.5}g", self.a_z_g);
-    }
-}
-
-fn process_packet(data: &[u8]) -> Option<Reading> {
-    if data.len() != 6 {
-        return None;
-    }
-
-    // Calibration constants, determined by experiment.
-    //
-    // TODO: Add a calibration mode that writes a file.
-
-    const G_SCALE_X: f32 = 17_700.0;
-    const G_SCALE_Y: f32 = 16_500.0;
-    const G_SCALE_Z: f32 = 17_300.0;
-
-    let a_x = (((data[0] as u16) << 8) | (data[1] as u16)) as i16;
-    let a_x_g = (a_x as f32) / G_SCALE_X;
-
-    let a_y = (((data[2] as u16) << 8) | (data[3] as u16)) as i16;
-    let a_y_g = (a_y as f32) / G_SCALE_Y;
-
-    let a_z = (((data[4] as u16) << 8) | (data[5] as u16)) as i16;
-    let a_z_g = (a_z as f32) / G_SCALE_Z;
-
-    Some(Reading {
-        a_x_g,
-        a_y_g,
-        a_z_g,
-    })
+    stop.store(true, Ordering::Relaxed);
+    handle.join().unwrap();
 }
